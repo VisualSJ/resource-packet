aab404106d42aca27ff00f67f1afea9c01a5d2b2
VisualSj <itharbors@outlook.com>
2015-09-25 18:25:24 +0800

    =。=

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
 var Fs = require("fs");
 var Path = require("path");
-
 var ConfigTemplate = require("./template/config.js");
 
-var SettingsDir = Editor. _type2profilepath.project;
-var configJson = Path.join(SettingsDir, "config.json");
-var groupJson = Path.join(SettingsDir, "group.json");
-if(Fs.existsSync(SettingsDir)){
-    console.log("true");
-}
+//定义一些配置的变量
+const configName = "packet-config.json";
+const groupName = "packet-group.json";
 
-var data = Fs.readFileSync(Editor.url('./config.json'), {encoding:'utf8'});
+var SettingsDir;//工程目录下的settings目录
+var configJson;//config文件的绝对路径
+var groupJson;//group文件的绝对路径
+
+var config = {};
+//var data = Fs.readFileSync(Editor.url('./config.json'), {encoding:'utf8'});
+
+var updateConfig = function(){
+    if(Fs.existsSync(configJson)){
+        //文件存在
+        console.log("read %s", configJson);
+    }else{
+        //配置文件不存在，创建配置
+        console.log("create %s", configJson);
+        Fs.writeFileSync(configJson, JSON.stringify(config));
+    }
+};
 
 module.exports = {
     'resource-packet:open': function(){
+        if(!SettingsDir)
+            SettingsDir = Editor. _type2profilepath.project;
+        if(!configJson)
+            configJson = Path.join(SettingsDir, configName);
+        if(!groupJson)
+            groupJson = Path.join(SettingsDir, groupName);
         Editor.Panel.open('resource-packet.panel');
+    },
+    'resource-packet:set-config': function(){
+        updateConfig();
+        Editor.sendToPanel('resource-packet.panel', 'resource-packet:set-config', config);
     }
 };
--- panel/resource-packet.js
+++ panel/resource-packet.js
@@ -2,10 +2,22 @@
     Editor.registerPanel( 'resource-packet.panel', {
         is: 'editor-resource-packet',
 
-        properties: {},
+        properties: {
+            test: {
+                type: String
+            }
+        },
 
-        ready: function () {},
+        ready: function () {
+            Editor.sendToCore('resource-packet:set-config');
+        },
 
         reload: function () {},
+
+        'resource-packet:set-config': function(config){
+            console.log(config);
+            this.config = config;
+            this.test = "123";
+        }
     });
 })();
